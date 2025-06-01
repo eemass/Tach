e5f9ecfc61a0532491ae48fb8eb1f66d8deb56a2
Samiisl <samiulislam0802@gmail.com>
2025-05-31 23:52:06 -0500

    Revert "readying for deployment"
    This reverts commit 1fbda8d7aa5c2039b09e92b6e27e2c225cb92092.

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -13,8 +12,6 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 
-const __dirname = path.resolve();
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
@@ -22,13 +19,6 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
